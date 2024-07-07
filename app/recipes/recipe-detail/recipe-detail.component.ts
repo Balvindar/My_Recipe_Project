367a1d874d922a69c2c3dc2feafdf63a57ef2d0f
Balvindar <73059145+Balvindar@users.noreply.github.com>
2024-07-07 17:25:09 +0200

    Add files via upload

---
+++ app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { RouterTestingHarness } from '@angular/router/testing';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent {
+  recipe!: Recipe;
+  id!: number;
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
+
+
+  ngOnInit() {
+    this.route.params.subscribe((param: Params) => {
+      this.id = +param['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    })
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+    // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route }); another way
+  }
+
+}
